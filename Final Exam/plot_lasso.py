8359e8af7301fcbab26eded81240f5a9fee603c2
Karan Patel <karanpatel31105115@gmail.com>
2017-04-28 11:06:47 +0530

    Add files via upload

---
+++ Final Exam/plot_lasso.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+print(__doc__)
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import linear_model
+from sklearn import datasets
+
+diabetes = datasets.load_diabetes() #Taken dataset as the diabetes which is in sklearn
+X = diabetes.data
+y = diabetes.target
+
+print("Computing regularization path using the LARS ...")
+alphas, _, coefs = linear_model.lars_path(X, y, method='lasso', verbose=True) #Taken lars algorithm
+
+xx = np.sum(np.abs(coefs.T), axis=1)
+xx /= xx[-1]
+
+plt.plot(xx, coefs.T)
+ymin, ymax = plt.ylim()
+plt.vlines(xx, ymin, ymax, linestyle='dashed')
+plt.xlabel('|coef| / max|coef|')
+plt.ylabel('Coefficients')
+plt.title('LASSO Path')
+plt.axis('tight')
+plt.show()
